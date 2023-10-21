575d9d5927ae12533a1af190d8a5bb9b1a4dad97
Dmitrii Semak <dmitru.semak@gmail.com>
2023-10-21 13:11:23 +0300

    Update description for the Sprint 6 Final B

--- Sprint6 Final/B/Rust/src/main.rs
+++ Sprint6 Final/B/Rust/src/main.rs
@@ -1,25 +1,44 @@
 /*
 -- WORKING PRINCIPLE --
-This program determines whether the given railway map is optimal. A railway map is represented as a graph, where cities
- are vertices and roads are edges. A railway map is considered optimal if, for any pair of cities A and B, it is
- possible to go from city A to city B only using roads of one type or if the route cannot be built at all.
+This program determines whether the presented railway map is optimal. The railway map is represented as a graph where
+ cities are vertices and roads are edges. A railway map is considered optimal if for any pair of cities A and B, it is
+ possible to travel from city A to city B using only roads of one type, or if a route cannot be constructed at all.
 
-The program uses the Depth First Search (DFS) algorithm to traverse the graph and detect whether there are any cycles,
- indicating that the map is not optimal. A cycle is detected when there is a back edge present in the graph.
+The program uses the Depth First Search (DFS) algorithm to traverse the graph and check that there are no two different
+ paths of different types (colors) between pairs of vertices.
 
 -- PROOF OF CORRECTNESS --
-The program correctly identifies whether the given railway map is optimal or not by performing the following steps:
-1. Represent the railway map as a directed graph.
-2. Traverse the graph using DFS and mark vertices with three colors to indicate their state.
-3. Detect any back edges during the DFS traversal to identify cycles in the graph.
+1. To determine the visited vertices, we will use the following colors:
+- WHITE - vertex has not been visited yet
+- GRAY - vertex has been visited, but not all its edges have been processed
+- BLACK - vertex has been visited and all its edges have been processed
 
-If no cycles are detected during the traversal, the railway map is considered optimal; otherwise, it is not optimal.
+2. If during the traversal of the graph we encounter a vertex of color GRAY, it means that there is a cycle in the
+ graph (the vertex was visited earlier in the current traversal, indicating a cycle).
+If there is a cycle in the graph, it indicates that there are multiple paths of different types (R and B) between some
+ cities, making the map non-optimal.
+
+3. Let's prove the following statement:
+The minimum cycle size in the graph from the problem statement is 3.
+
+We will prove by induction. Suppose there is a cycle A1 -> A2 -> ... -> Am -> A1.
+
+Base of induction: m == 3, for m < 3 we cannot obtain a cycle based on the problem statement (construction).
+A1 -> A2 -> A3 -> A1. It's evident that city A3 can be reached in two ways:
+   - From city A1 through city A2 by roads of type R.
+   - From city A1 directly by a road of type B.
+And such a graph is non-optimal based on the problem statement.
+
+Induction step: m == k. Consider the edge connecting vertices A1 and A3.
+   - If A3 -> A1, then this case is already proven in the base of induction.
+   - If A1 -> A3, then we get a cycle A1 -> A3 -> ... -> Am -> A1, which is 1 less in length.
+   Repeating the process further, we come to a cycle of length 3.
 
 -- TIME COMPLEXITY --
-The time complexity of the DFS traversal is O(V + E), where V is the number of vertices (cities), and E is the number of edges (roads).
+The time complexity of the DFS traversal is O(V + E), where V is the number of vertices (cities) and E is the number of edges (roads).
 
 -- SPACE COMPLEXITY --
-The space complexity of this program is O(V + E) due to the storage required for the graph representation and the visited array.
+The space complexity of this program is O(V + E) due to the need to store the graph and the array of visited vertices.
 
 Link to successful report: https://contest.yandex.ru/contest/25070/run-report/91164697/
 */
